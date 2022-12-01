addb4c309787761fa5f503bc026f8d0cc10761d0
Cory W <12cwarden@gmail.com>
2022-12-01 18:56:47 -0500

    update for pull

--- src/components/account-details/AccountDetails.tsx
+++ src/components/account-details/AccountDetails.tsx
@@ -1,24 +1,26 @@
+import Button from '@mui/material/Button';
 import React, { useEffect, useState } from 'react';
-import { useContext } from 'react';
-import { UserContext } from '../../context/user.context';
-import { Transaction } from '../../models/Transaction';
-import { apiGetAccounts, apiGetTransactions } from '../../remote/banking-api/account.api';
-import Navbar from '../navbar/Navbar';
 import { useNavigate } from 'react-router-dom';
+import { Transaction } from '../../models/Transaction';
+import {
+  apiGetAccounts,
+  apiGetTransactions,
+} from '../../remote/banking-api/account.api';
+import Navbar from '../navbar/Navbar';
 import './AccountDetails.css';
 import CreateTransactionForm from './CreateTransactionForm';
-import { Account } from '../../models/Account';
-import Button from '@mui/material/Button';
-import StyledTable from './StyledTable';
 import SideBar from './SideBar';
+import StyledTable from './StyledTable';
 
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
 
 export default function AccountDetails() {
   const navigate = useNavigate();
   const user = useAppSelector((state) => state.user.user);
   const [transaction, setTransactions] = useState<Transaction[]>([]);
-  const currentAccount = useAppSelector((state) => state.account.currentAccount);
+  const currentAccount = useAppSelector(
+    (state) => state.account.currentAccount
+  );
   let txnForm = <></>;
 
   useEffect(() => {
@@ -58,25 +60,24 @@ export default function AccountDetails() {
     <>
       <Navbar />
       <div className={'top-container'}>
-      <SideBar />
-      <div className="account-wrap">
-        <div className="account-details">
-
-          <h2>{currentAccount.name}</h2>
-          <h1>{currentAccount.balance}</h1>
-          <Button
-            onClick={() => {
-              navigate('/');
-            }}
-          >
-            Go Back
-          </Button>
+        <SideBar />
+        <div className="account-wrap">
+          <div className="account-details">
+            <h2>{currentAccount.name}</h2>
+            <h1>{currentAccount.balance}</h1>
+            <Button
+              onClick={() => {
+                navigate('/');
+              }}
+            >
+              Go Back
+            </Button>
           </div>
         </div>
       </div>
-      <div className='txn-wrap'>
-        <h1 className='title'>Recent Transactions</h1>
-        <StyledTable transaction={transaction}/>
+      <div className="txn-wrap">
+        <h1 className="title">Recent Transactions</h1>
+        <StyledTable transaction={transaction} />
       </div>
     </>
   );
