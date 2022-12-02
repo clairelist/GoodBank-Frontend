c595dd8035204ed30b1ed218d4a4e6ddb375ee43
clairelist <claire.list2501@gmail.com>
2022-12-02 14:59:16 -0800

    reset password frontend start

---
+++ src/components/reset-password/ResetPassword.tsx
@@ -0,0 +1,14 @@
+//I take in an email and password
+//and make a PATCH request to the API, to update the password.
+
+import { useState } from 'react';
+
+function ResetPassword(){
+    return (
+        <div>
+            <h1>I am the RESET PASSWORD COMPONENT!</h1>
+        </div>
+    )
+}
+
+export default ResetPassword;
