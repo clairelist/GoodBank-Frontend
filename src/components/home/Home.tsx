4b6424ae70b98779873a2a93cf5dabe42eadedb8
Torie K <victoria.konkiel@yahoo.com>
2022-12-02 10:02:59 -0600

    fixed merge conflict with dev

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -3,7 +3,7 @@ import Accounts from '../account-details/Accounts';
 import CreditCard from '../credit-cards/CreditCard';
 import Loans from '../account-details/Loans';
 
-
+//Fixed merge conflict
 export default function Home() {
 
   return (
