10a8f01b979070c5d70c258d07dfb9d4045a85c8
Jonny Andino <66392799+bluephosphor@users.noreply.github.com>
2022-12-02 11:15:57 -0500

    mark as seen is functional

--- src/components/navbar/notifications/NotificationList.tsx
+++ src/components/navbar/notifications/NotificationList.tsx
@@ -1,25 +1,10 @@
 import { Box, List } from '@mui/material';
-import { useEffect} from 'react';
-import { useAppDispatch, useAppSelector } from '../../../app/hooks';
-import { apiGetUserNotifications } from '../../../remote/banking-api/notification.api';
+import { useAppSelector } from '../../../app/hooks';
 import NotificationItem from './NotificationItem';
-import { setUserNotifications } from '../../../features/notification/notificationSlice';
 
 export default function NotificationList() {
-
-  const user = useAppSelector(state => state.user.user);
+  
   const notifications = useAppSelector(state => state.notifications.list);
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    const fetchNotifs = async () => {
-      if (user) {
-        const result = await apiGetUserNotifications(user.id);
-        dispatch(setUserNotifications(result.payload));
-      }
-    };
-    fetchNotifs();
-  }, [user]);
 
   if (!notifications) return <>loading...</>;
 
