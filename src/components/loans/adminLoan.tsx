b6b6af7c1dd1d523081e592d8bb49b2f04ab612d
Cory Warden <12cwarden@gmail.com>
2022-12-02 22:39:30 -0500

    fixed final smells

--- src/components/loans/adminLoan.tsx
+++ src/components/loans/adminLoan.tsx
@@ -1,13 +1,12 @@
-import { useAppSelector } from '../../app/hooks';
 import Button from '@mui/material/Button';
-import { useNavigate } from 'react-router-dom';
 import Paper from '@mui/material/Paper';
 import { useEffect, useState } from 'react';
-import { apiChangeStatus, apiGetPendingLoans } from '../../remote/banking-api/loan.api';
+import { useAppSelector } from '../../app/hooks';
 import { LoanDetails } from '../../models/LoanDetails';
-import Navbar from '../navbar/Navbar';
-import { accountSlice } from '../../features/account/accountSlice';
-
+import {
+  apiChangeStatus,
+  apiGetPendingLoans,
+} from '../../remote/banking-api/loan.api';
 
 const AdminLoan = () => {
   const user = useAppSelector((state) => state.user.user);
@@ -30,26 +29,44 @@ const AdminLoan = () => {
     currentLoan.status = status;
     const response = await apiChangeStatus(currentLoan);
     console.log(response.payload);
-    setLoans(loans.filter((x: LoanDetails) => x.loanID !== response.payload.loanID))
-  }
-  return( <Paper>
-    
-    <h1>this is just a test hahahahahahah!!!! (Pending Loans)</h1>
-    {loans.length > 0? loans.map((loan:LoanDetails)=>(
-    <div key={loan.loanID+1}>
-    <p>LoanID: {loan.loanID}</p>
-    <p>LoanUserID: {loan.userId}</p>
-    <p>LoanReason: {loan.reason}</p>
-    <p>LoanInitialAmount: {loan.initialAmount}</p>
-    <p>CreationDate: {loan.creationDate.toString()}</p>
-    <div>
-      <Button variant="contained" color="primary" size="small" onClick={() => handleStatus(loan, "APPROVED")}>Approve</Button>
-      <Button variant="contained" color="primary" size="small" onClick={() => handleStatus(loan, "DENIED")}>Deny</Button>
-    </div>
-    </div>
-    )):''}
+    setLoans(
+      loans.filter((x: LoanDetails) => x.loanID !== response.payload.loanID)
+    );
+  };
+  return (
+    <Paper>
+      <h1>this is just a test hahahahahahah!!!! (Pending Loans)</h1>
+      {loans.length > 0
+        ? loans.map((loan: LoanDetails) => (
+            <div key={loan.loanID + 1}>
+              <p>LoanID: {loan.loanID}</p>
+              <p>LoanUserID: {loan.userId}</p>
+              <p>LoanReason: {loan.reason}</p>
+              <p>LoanInitialAmount: {loan.initialAmount}</p>
+              <p>CreationDate: {loan.creationDate.toString()}</p>
+              <div>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="small"
+                  onClick={() => handleStatus(loan, 'APPROVED')}
+                >
+                  Approve
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="small"
+                  onClick={() => handleStatus(loan, 'DENIED')}
+                >
+                  Deny
+                </Button>
+              </div>
+            </div>
+          ))
+        : ''}
     </Paper>
-        );
+  );
 };
 
 export default AdminLoan;
