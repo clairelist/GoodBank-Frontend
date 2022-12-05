4b9f2887f80a779c087f9e5a261938efd1ff8960
clairelist <claire.list2501@gmail.com>
2022-12-05 11:00:02 -0800

    well it renders, time for lunch

--- src/components/reset-password/ResetPassword.tsx
+++ src/components/reset-password/ResetPassword.tsx
@@ -1,14 +1,14 @@
 import bankingClient from '../../remote/banking-api/bankingClient';
 import { SyntheticEvent, useState, } from 'react';
 
-function ResetPassword(props:any){
+function ResetPassword(){
 
-    const {email} = props;
+    //const {email} = props;
 
     const [password, setPassword] = useState("");
     const [submission, setSubmission] = useState({});
 
-    setSubmission({email: email, password: password});
+    //setSubmission({email: email, password: password});
 
     const handleChange = (e: SyntheticEvent) => { //[(e.target as HTMLInputElement).name]: (e.target as HTMLInputElement)
       //  .value,
