5fbe5de40049a2f25b0e7bab20b821d0a7ba2d9b
clairelist <claire.list2501@gmail.com>
2022-12-05 09:49:27 -0800

    some more tessts

--- src/components/reset-password/resetPassword.test.tsx
+++ src/components/reset-password/resetPassword.test.tsx
@@ -1,25 +1,28 @@
 //I AM THE TESTS FOR RESET PASSWORD FLOW
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import ResetPassword from './ResetPassword';
 import Login from '../login/Login';
 
 describe('Reset password flow', ()=>{
     it('login page should contain a link to reset password', ()=>{
-        //Arrange, Act, Assert
         render(<Login />);
-
-        const aLink = screen.getByText('Forgot password?');
-
+        let aLink = screen.getByText('Forgot password?');
         expect(aLink).toBeInTheDocument();
     });
 
     it('clicking forgot password should render reset password component', ()=>{
-
+        render(<Login />);
+        fireEvent.click(screen.getByText('Forgot password?'));
+        let mockPass = screen.getByLabelText('password');
+        expect(mockPass).toBeInTheDocument();
     });
 
     it('should be able to type in a new password in the field', ()=>{
-
+        render(<ResetPassword />);
+        let mockField = screen.getByLabelText('password');
+        fireEvent.change(mockField, {target: {value: 'marimite'}});
+        expect((mockField as HTMLInputElement).value).toBe('marimite');
     });
 
     it('clicking submit should call axios', ()=>{
