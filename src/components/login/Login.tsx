307d030f009a459dffec74faf8b83e77ab4793c4
Cory Warden <12cwarden@gmail.com>
2022-12-08 16:49:01 -0500

    added validation and error for login

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,4 +1,5 @@
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { Alert } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -9,6 +10,7 @@ import Link from '@mui/material/Link';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
+import { useState } from 'react';
 import { Link as Rlink, useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { signIn } from '../../features/user/userSlice';
@@ -18,24 +20,23 @@ export default function Login() {
   const dispatch = useAppDispatch();
   const user = useAppSelector((state) => state.user.user);
   const navigate = useNavigate();
+  const [error, setError] = useState('');
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    try {
-      const data = new FormData(event.currentTarget);
-      const email = data.get('email');
-      const password = data.get('password');
+    const data = new FormData(event.currentTarget);
+    const email = data.get('email');
+    const password = data.get('password');
 
-      const response = await apiLogin(`${email}`, `${password}`);
-      if (response.status >= 200 && response.status < 300) {
-        let token = response.headers['authorization'];
-        if (token) {
-          sessionStorage.setItem('token', token);
-        }
-        dispatch(signIn(response.payload));
+    const response = await apiLogin(`${email}`, `${password}`);
+    if (response.status === 200) {
+      let token = response.headers['authorization'];
+      if (token) {
+        sessionStorage.setItem('token', token);
       }
-    } catch (error) {
-      return <p>Invalid Credentials!</p>;
+      dispatch(signIn(response.payload));
+    } else {
+      setError('Invalid credentials!');
     }
   };
 
@@ -84,6 +85,7 @@ export default function Login() {
             id="password"
             autoComplete="current-password"
           />
+          <Alert severity="error">{error}</Alert>
           <Rlink to={'reset-password'}>Forgot password?</Rlink>
           <Button
             type="submit"
