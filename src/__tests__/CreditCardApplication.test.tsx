237c44f7b9dfba52e014a7882cae0ac1992303e9
Torie K <victoria.konkiel@yahoo.com>
2022-12-13 09:41:45 -0600

    not logged in test

--- src/__tests__/CreditCardApplication.test.tsx
+++ src/__tests__/CreditCardApplication.test.tsx
@@ -1,8 +1,8 @@
-import {render, screen} from '@testing-library/react';
+import {render} from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../app/store';
 import CreditCardApplication from '../components/credit-cards/CreditCardApplication';
-import configureStore from 'redux-mock-store';
+
 
 const mockedUsedNavigate = jest.fn();
     jest.mock('react-router-dom', () => ({
@@ -11,17 +11,11 @@ const mockedUsedNavigate = jest.fn();
     }));
 
 describe('Credit Card Application Form test suite', () => {
-    // beforeEach(() => {
-    //     const verify = jest.spyOn(jwt, 'verify');
-    //     verify.mockImplementation(() => () => ({ verified: 'true' }));
-    // })
-    // });
-    it('Requested Amount starts out blank', () => {
-        render(<Provider store={store}><CreditCardApplication/></Provider>);
-        const requestedAmount = screen.getByDisplayValue("Requested Amount*");
-        expect((requestedAmount as HTMLInputElement).value).toBeUndefined;
-        // expect(documentBody.getByText("Requested Amount*")).toBeInTheDocument()
-        
+
+    it('should verify user is not logged in', () => {
+        const {getByText} = render(<Provider store={store}><CreditCardApplication /></Provider>);
+        const titleValue = getByText('Pending Loans');
+        expect(titleValue).toHaveTextContent('Pending Loans');
     })
 
 })
