d11dc38a2f0e7b20ef2bb856baea6454c3664071
Lukas R <garff247@gmail.com>
2022-12-12 19:59:57 -0800

    fixed reset password

--- src/components/reset-password/ResetPassword.tsx
+++ src/components/reset-password/ResetPassword.tsx
@@ -4,61 +4,63 @@ import TextField from '@mui/material/TextField';
 
 import bankingClient from '../../remote/banking-api/bankingClient';
 import { SyntheticEvent, useState, } from 'react';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-function ResetPassword(){
-    const [submission, setSubmission] = useState<any>({email: '', password: ''});
-    const [newPassword, setNewPassword] = useState(''); //used for password CONFIRMATION
-    const [error, setError] = useState(false);
-    const [confirmation, setConfirmation] = useState(false);
-    const navigate: any = useNavigate(); //Dear typescript, stop it. Get some help.
+function ResetPassword() {
+  const [submission, setSubmission] = useState<any>({ email: '', password: '' });
+  const [newPassword, setNewPassword] = useState(''); //used for password CONFIRMATION
+  const [error, setError] = useState(false);
+  const [confirmation, setConfirmation] = useState(false);
+  const navigate: any = useNavigate(); //Dear typescript, stop it. Get some help.
 
-    const navAfterTime = () => { 
-        //@DOCS: used for the timeout, below, so our confirm message is displayed.
-        navigate('/login');
-    }
+  const navAfterTime = () => {
+    //@DOCS: used for the timeout, below, so our confirm message is displayed.
+    navigate('/login');
+  }
 
-    const validate=(value: string, value2: string)=>{
-        // ADD MIN LENGTH REQS HERE AND IN REGISTRATION
-        if(value !== value2){
-            setError(true);
-            return true;
-            //NOTE: must both set the error, and return a boolean
-            //so that our check in submit handler works, below.
-        } else {
-            setError(false);
-            return false;
-        }
+  const validate = (value: string, value2: string) => {
+    // ADD MIN LENGTH REQS HERE AND IN REGISTRATION
+    if (value !== value2) {
+      setError(true);
+      return true;
+      //NOTE: must both set the error, and return a boolean
+      //so that our check in submit handler works, below.
+    } else {
+      setError(false);
+      return false;
     }
+  }
 
-    const handleChange = (e: SyntheticEvent) => {
-      setSubmission({
-        ...submission,
-        [(e.target as HTMLInputElement).name]: (e.target as HTMLInputElement)
-          .value,
-      });
+  const handleChange = (e: SyntheticEvent) => {
+    setSubmission({
+      ...submission,
+      [(e.target as HTMLInputElement).name]: (e.target as HTMLInputElement)
+        .value,
+    });
+  }
+  const handleNewPassChange = (e: SyntheticEvent) => {
+    //used for confirmation that passwords are the same!
+    setNewPassword((e.target as HTMLInputElement).value);
+  }
+  const handleSubmit = () => {
+    console.log(submission);
+    if (validate(submission.password, newPassword) === true) {
+      console.log("do they match?")
+      return null;
+    } else {
+      bankingClient.patch('/user/reset-password', {email: submission.email, password: submission.password, confirmPassword: newPassword})
+        .then(res => {
+          setConfirmation(true);
+          setTimeout(navAfterTime, 1500);
+        })
+        .catch(err => {
+          setError(true);
+        })
     }
-    const handleNewPassChange = (e: SyntheticEvent) => { 
-        //used for confirmation that passwords are the same!
-        setNewPassword((e.target as HTMLInputElement).value);
-    }
-    const handleSubmit = () => {
-        if(validate(submission.password, newPassword) === true){
-            return null;
-        } else {
-            bankingClient.patch('/user/reset-password', submission)
-            .then(res=>{
-                setConfirmation(true);
-                setTimeout(navAfterTime, 1500);
-            })
-            .catch(err=>{
-                setError(true);
-            })
-        }
-}
-    return (
+  }
+  return (
     <div>
-        <Box
+      <Box
         sx={{
           marginTop: 8,
           display: 'flex',
@@ -66,8 +68,8 @@ function ResetPassword(){
           alignItems: 'center',
         }}
       >
-    <h2>Please fill me out to submit a password reset request.</h2>
-    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <h2>Please fill me out to submit a password reset request.</h2>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
           <TextField
             margin="normal"
             required
@@ -104,20 +106,20 @@ function ResetPassword(){
             value={newPassword}
             onChange={handleNewPassChange}
           />
-          </Box>
-          {error ? <p>"Passwords MUST match, AND you must provide a valid EMAIL!"</p> : <></>}
-          {confirmation ? <p>Your password has been RESET.</p> : 
-              <Button
-              type="submit"
-              color="secondary"
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={handleSubmit}
-              >Submit</Button>
-          }
-            </Box>
+        </Box>
+        {error ? <p>"Passwords MUST match, AND you must provide a valid EMAIL!"</p> : <></>}
+        {confirmation ? <p>Your password has been RESET.</p> :
+          <Button
+            type="submit"
+            color="secondary"
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            onClick={handleSubmit}
+          >Submit</Button>
+        }
+      </Box>
     </div>
-    )
+  )
 }
 
 export default ResetPassword;
