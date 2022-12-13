a16a3d16de5f9a4ee6509870dc3f4e8c90a5d290
Lukas R <garff247@gmail.com>
2022-12-13 12:43:51 -0800

    created test for updated profile

---
+++ src/__tests__/update.api.spec.ts
@@ -0,0 +1,36 @@
+import bankingClient from "../remote/banking-api/bankingClient";
+import { apiUpdate } from '../remote/banking-api/update.api';
+import { User, UserUpdateRequest } from '../models/user';
+
+jest.mock('../remote/banking-api/bankingClient');
+const bankingClientMock = bankingClient as jest.Mocked<typeof bankingClient>;
+
+describe('Update user information', () => {
+  it('Should return updated user information', async () => {
+    (bankingClientMock.patch as jest.MockedFunction<typeof bankingClient.patch>).mockResolvedValue({
+      status: 200
+    })
+
+    let token = "token";
+    let testuser: UserUpdateRequest = {
+      id: 1,
+      email: "testuser@gmail.com",
+      firstName: "Lukas",
+      lastName: "Roberts",
+      address: "123 El Dorado",
+      city: "Hills",
+      state: "California",
+      zip: 95762
+    };
+
+    const result = await apiUpdate(testuser, token);
+
+    expect(bankingClient.patch).toHaveBeenCalledWith('/user/profile',
+      testuser,
+      {headers: { 'authorization': token},
+      withCredentials: true
+    })
+    expect(result.status).toBe(200);
+
+  })
+})
